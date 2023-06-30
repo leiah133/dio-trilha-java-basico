e14aa59aae5ab7ad116e840c98ebc739fbb8ecbf
leiah133 <leiaanjos87@gmail.com>
2023-06-30 18:41:39 -0300

    smarttv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,13 @@
 
 public class TiposVariaveis{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        System.out.println(numeroCurto2);
     }
 }
